10f0e767e337a651560f8e2a5d8804dc5becb612
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-03-24 15:39:14 +0000

    add PHPUnit Test Scaffolding

---
+++ tests/wpunit/ExampleWPUnitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NewfoldLabs\WP\Module\Loader;
+
+/**
+ * Example Unit test.
+ *
+ * @coversDefaultClass \NewfoldLabs\WP\Module\Loader\Example
+ */
+class ExampleWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
+
+}
